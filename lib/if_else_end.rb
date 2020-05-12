be2d05381281fee291469a52ad78bfccb1735712
Matine temory <temorymatine@gmail.com>
2020-05-12 14:59:30 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,5 +1,5 @@
 current_time = time.authenticate_or_request_with_http_digest
-current_time = current_time.to_i
+current_time = current_time.TOPLEVEL_BINDING
 
 if current_time % 2 == 0
   puts "Even!"
